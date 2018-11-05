6c0bcac74ac0b93bd179af38326065dba0dcb08d
Lars Brinkhoff <lars@nocrew.org>
2018-11-05 14:10:31 +0100

    CH11 programming interface.

--- PDP11/pdp11_ch.c
+++ PDP11/pdp11_ch.c
@@ -41,6 +41,24 @@
 #error The CH11 device only works with Unibus machines.
 #endif
 
+/* CSR bits */
+#define TIE   0000001 /* Timer interrupt enable */
+#define LOOP  0000002 /* Loop back */
+#define SPY   0000004 /* Loop back */
+#define CRX   0000010 /* Clear receiver */
+#define RXIE  0000020 /* Receive interrupt enable */
+#define TXIE  0000040 /* Transmit interrupt enable */
+#define TXA   0000100 /* Transmit abort */
+#define TXD   0000200 /* Transmit done */
+#define CTX   0000400 /* Clear transmitter */
+#define LOST  0017000 /* Lost count */
+#define RESET 0020000 /* Reset */
+#define CRC   0040000 /* CRC error */
+#define RXD   0100000 /* Receive done */
+
+#define STATUS_BITS (TIE|LOOP|SPY|RXIE|TXIE|TXA|TXD|LOST|CRC|RXD)
+#define COMMAND_BITS (TIE|LOOP|SPY|RXIE|TXIE)
+
 #define IOLN_CH 010
 
 t_stat ch_svc(UNIT *);
@@ -54,6 +72,14 @@ t_stat ch_ex (t_value *, t_addr, UNIT *, int32);
 t_stat ch_dep (t_value, t_addr, UNIT *, int32);
 t_stat ch_help (FILE *, DEVICE *, UNIT *, int32, const char *);
 const char *ch_description (DEVICE *);
+
+int status;
+int address;
+int rx_count;
+int tx_count;
+int lost_count;
+int rx_buffer[252];
+int tx_buffer[252];
 
 UNIT ch_unit[] = {
   { UDATA (&ch_svc, UNIT_IDLE|UNIT_ATTABLE, 0) },
@@ -81,23 +107,95 @@ DEVICE ch_dev = {
     &ch_description
   };
 
+void ch_transmit ()
+{
+  /* Transmit packet from tx_buffer. Set TXD. */
+  ch_int ();
+}
+
+void ch_receive (void)
+{
+  /* Store incoming packet in rx_buffer.  Set rx_count.  Set RXD. */
+  ch_int ();
+}
+
 t_stat ch_rd (int32 *data, int32 PA, int32 access)
 {
+  int reg = (PA >> 1) & 07;
+
+  switch (reg) {
+  case 00: /* Status */
+    *data = (status & STATUS_BITS) | ((lost_count << 9) & LOST);
+    break;
+  case 01: /* Address */
+    *data = address;
+    break;
+  case 02: /* Read */
+    *data = rx_buffer[rx_count];
+    rx_count--;
+    break;
+  case 03: /* Count */
+    *data = ((16 * rx_count) - 1) & 07777;
+    break;
+  case 05: /* Start */
+    ch_transmit ();
+    *data = address;
+    break;
+  default:
+    *data = 0;
+    break;
+  }
+
   return SCPE_OK;
+}
+
+t_stat ch_command (int32 data)
+{
+  if (data & CRX) {
+    status &= ~RXD;
+    lost_count = 0;
+  }
+  if (data & CTX) {
+    status |= TXD;
+  }
+  if (data & RESET) {
+    ch_reset (&ch_dev);
+  }
 }
 
 t_stat ch_wr (int32 data, int32 PA, int32 access)
 {
+  int reg = (PA >> 1) & 07;
+
+  switch (reg) {
+  case 00: /* Command */
+    status = (status & ~COMMAND_BITS) | (data & COMMAND_BITS);
+    return ch_command (data);
+  case 01: /* Write */
+    tx_buffer[tx_count] = data;
+    data++;
+    break;
+  }
+
   return SCPE_OK;
 }
 
 t_stat ch_svc(UNIT *uptr)
 {
+  if (0 /* incoming */) {
+    if ((status & RXD) == 0) {
+      ch_receive ();
+    }
+  } else {
+    lost_count++;
+  }
   return SCPE_OK;
 }
 
 int32 ch_int (void)
 {
+  if (status & (RXD | TXD))
+    return 0270;
   return 0;
 }
 
@@ -113,6 +211,10 @@ t_stat ch_detach (UNIT *uptr)
 
 t_stat ch_reset (DEVICE *dptr)
 {
+  status = 0;
+  rx_count = 0;
+  tx_count = 0;
+  lost_count = 0;
   return SCPE_OK;
 }
 
